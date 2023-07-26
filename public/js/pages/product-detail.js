eb262677287632c12572a57523cd7c3af7b363cc
[SW5] 홍승진 <cmikal47@gmail.com>
2023-07-26 18:41:09 +0900

    feat: product-detail 페이지 수량 변경, 총금액 기능 추가

--- public/js/pages/product-detail.js
+++ public/js/pages/product-detail.js
@@ -1,2 +1,46 @@
 import setupHeader from '../components/header.js';
-setupHeader();
+import { TOAST_TYPES } from '../utils/constants.js';
+import renderToastMessage from '../components/toast-message.js';
+
+// 총 금액 업데이트
+const updateTotalPrice = (quantity = 1) => {
+  const productPrice = Number(
+    document.getElementById('productPrice').dataset.price
+  );
+  const totalPrice = productPrice * quantity;
+
+  const totalPriceElement = document.getElementById('totalPrice');
+  totalPriceElement.textContent = `${totalPrice.toLocaleString()}원`;
+};
+
+// 수량 조절
+const adjustQuantity = (e) => {
+  const quantityElement = document.getElementById('quantity');
+  let quantity = Number(quantityElement.textContent);
+
+  if (e.target.id === 'increaseBtn') {
+    quantity++;
+  } else if (e.target.id === 'decreaseBtn' && quantity > 1) {
+    quantity--;
+  }
+
+  quantityElement.textContent = quantity;
+  updateTotalPrice(quantity);
+};
+
+// 수량 버튼 이벤트 바인딩
+const bindEventsCountBtns = () => {
+  const decreaseBtn = document.getElementById('decreaseBtn');
+  const increaseBtn = document.getElementById('increaseBtn');
+
+  decreaseBtn.addEventListener('click', adjustQuantity);
+  increaseBtn.addEventListener('click', adjustQuantity);
+};
+
+const initializeModule = () => {
+  setupHeader();
+  bindEventsCountBtns();
+  updateTotalPrice();
+};
+
+initializeModule();
