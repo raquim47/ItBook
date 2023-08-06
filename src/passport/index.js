a3b8acf1c4bacf90309383bc122d641f0103aaa1
[SW5] 홍승진 <cmikal47@gmail.com>
2023-08-07 06:19:05 +0900

    refactor : passport/strategies/local 제거

--- src/passport/index.js
+++ src/passport/index.js
@@ -1,9 +1,6 @@
 import passport from 'passport';
-import local from './strategies/local';
 import jwt from './strategies/jwt';
 
 export default () => {
-  passport.use(local);
-  // jwt strategy 사용
   passport.use(jwt);
 };
